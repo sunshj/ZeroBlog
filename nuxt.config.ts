7cd6ff95abc8e51ed0bcc9b8c415b92c09391943
sunshj <47186980+sunshj@users.noreply.github.com>
2026-03-15 21:48:07 +0800

    feat: add nitro configuration with minify and sourceMap options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  nitro: {
+    minify: true,
+    sourceMap: true
+  },
 
   css: ['~/assets/base.css', '~/assets/transitions.css'],
 
